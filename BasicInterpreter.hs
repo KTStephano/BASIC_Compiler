f0a75778a3d9c0840b2b0f586c31c3b1496b4a1a
KTStephano <amunra@unm.edu>
2017-11-26 17:22:57 -0700

    Adding initial files

---
+++ BasicInterpreter.hs
@@ -0,0 +1,155 @@
+-- * = 0 or mroe
+-- + = 1 or more
+-- {} = symbolic class or something
+-- lower = lowercase digit
+-- digit = number
+
+import Parselib
+import Data.Char
+import Control.Monad
+import Control.Applicative hiding (many)
+
+data Sexpr = Symbol String | Number Int | Floating Double | Nil | Cons Sexpr Sexpr deriving (Eq)
+
+instance Show Sexpr where
+    show (Symbol x) = x
+    show (Number x) = show x
+    show (Floating x) = show x
+    show Nil = "()"
+    show (Cons x y) = "(" ++ show x ++ showCdr y ++ ")"
+
+showCdr :: Sexpr -> String
+showCdr Nil = ""
+showCdr (Cons x Nil) = " " ++ show x
+showCdr (Cons x v@(Cons y z)) = " " ++ show x ++ showCdr v
+showCdr (Cons x y) = " " ++ show x ++ " . " ++ show y
+showCdr x = " . " ++ show x
+
+{-
+
+{symbol} ::= {first} {symbolic}* | {string}
+{first} ::= {misc} | {lower}
+{symbolic} ::= {first} | {digit}
+{misc} ::= ’<’ | ’>’ | ’ˆ’ | ’+’ | ’-’ | ’*’ | ’/’ | ’=’
+{integernum} ::= {digit}+
+{number} ::= {digit}+ . {digit}+
+S ::= () | (E) | '(E) | A | (S . S)
+E ::= (E)E | S E | S                --> This returns a pair of sexprs
+A ::= {symbol} | {number} | {integernum}
+
+-}
+
+quote = do
+    x <- symb "\""
+    y <- (many quotedString)
+    z <- symb "\""
+    return (x ++ y ++ z)
+
+quotedString = alphanum +++ (sat isSpace)
+
+cdigit = do 
+    c <- sat isDigit
+    return c
+
+integernum = do
+    r <- many1 cdigit
+    return (read r :: Int)
+
+number = do
+    r <- many1 cdigit
+    d <- symb "."
+    l <- many1 cdigit
+    return (read (r ++ d ++ l) :: Double)
+
+misc = do
+    r <- item--token item
+    let miscVals = ['<', '>', '^', '+', '-', '*', '/', '=']
+    if (r `elem` miscVals) then return r else mzero
+
+first = misc +++ lower
+
+symbolic = first +++ cdigit --(token first) +++ (token cdigit)
+
+symbol = (do
+    f <- first
+    s <- token (many symbolic)
+    return (f:s)) +++ quote
+
+nil = do
+    symb "("
+    symb ")"
+    return Nil
+
+wrappedE = do
+    (symb "(" +++ symb "'(")
+    res <- token e
+    symb ")"
+    return res
+
+ss = do
+    symb "("
+    left <- token s
+    symb "."
+    right <- token s
+    symb ")"
+    return $ Cons left right
+    
+ee = do
+    res <- wrappedE
+    recur <- token e
+    return $ Cons res recur
+
+se = do
+    left <- token s
+    right <- token e
+    return $ Cons left right
+
+s = nil +++ wrappedE +++ a +++ ss
+
+e = ee +++ se +++ (do {res <- s; return $ Cons res Nil})
+
+--A :: Parser Sexpr
+a :: Parser Sexpr
+a = (do
+    s <- symbol
+    return $ Symbol s) +++
+        (do
+            n <- number
+            return $ Floating n) +++
+            (do
+                n <- integernum
+                return $ Number n)
+
+p str = let result = parse s str
+        in if (result == []) then Symbol "Error parsing string"
+           else fst (result !! 0)
+
+foo = "(define foo " ++
+      "'((100 input \"What is the value of A\" a )" ++
+      " (110 input \"What is the value of B\" b )" ++
+      " (120 input \"What is the value of C\" c )" ++
+      " (130 let d = ((b * b) - (4.0 * (a * c))) )" ++
+      " (140 print d) (150 end)))"
+
+data Value = VIntegral Int | VFloating Double | VString String deriving (Show, Eq)
+
+newtype Environment = Environment {getEnv :: [(String, Value)]} deriving (Show, Eq)
+
+findEnv :: Environment -> String -> [(String, Value)]
+findEnv (Environment []) _ = []
+findEnv (Environment ((s,v):xs)) str = if s == str then [(s,v)] else findEnv (Environment xs) str
+
+updateEnv :: Environment -> String -> Value -> Environment
+updateEnv env str val = let x = findEnv env str
+                        in if (x == []) then Environment $ (getEnv env) ++ [(str, val)]
+                           else Environment $ (filter (\y -> y /= (x !! 0)) $ getEnv env) ++ [(str, val)]
+
+data Bytecode = Return {line :: Int} | Push {arg :: Value} | Pop | Print {line :: Int} deriving (Show)
+
+extractFunction line (Cons (Symbol "return") s) = Return line
+extractFunction line (Cons (Symbol "print") s) = Print line
+
+compile :: Sexpr -> [Bytecode] -> [Bytecode]
+compile (Cons (Number i) s2) code = code
+compile (Cons (Cons (Number i) s1) s2)
+compile (Cons s1 s2) code = compile s2 code
---
+++ Parselib.hs
@@ -0,0 +1,160 @@
+{---------------------------------------------------------------------
+
+           A HASKELL LIBRARY OF MONADIC PARSER COMBINATORS
+
+                          17th April 1997
+
+               Graham Hutton              Erik Meijer
+          University of Nottingham   University of Utrecht
+
+This Haskell 1.3 library is derived from our forthcoming JFP article
+"Monadic Parsing in Haskell".  The library also includes a few extra
+combinators that were not discussed in the article for reasons of space:
+
+   o force (used to make "many" deliver results lazily);
+
+   o digit, lower, upper, letter, alphanum (useful parsers);
+
+   o ident, nat, int (useful token parsers).
+
+---------------------------------------------------------------------}
+
+module Parselib
+(Parser, item, sat, (+++), string, many, many1, sepby, sepby1,
+ chainl, chainl1, char, digit, lower, upper, letter, alphanum,
+ symb, ident, nat, int, token, apply, parse, space, integer, natural) where
+
+import Data.Char
+import Control.Monad
+import Control.Applicative hiding (many)
+
+infixr 5 +++
+
+-- Monad of parsers: -------------------------------------------------
+
+newtype Parser a = Parser (String -> [(a,String)])
+
+instance Alternative Parser where
+  (<|>) = mplus
+  empty = mzero
+
+instance Functor Parser where
+  fmap = (<$>)
+
+instance Applicative Parser where
+  pure = return
+  (<*>) = ap
+
+instance Monad Parser where
+   return a      = Parser (\cs -> [(a,cs)])
+   p >>= f       = Parser (\cs -> concat [parse (f a) cs' |
+                                     (a,cs') <- parse p cs])
+
+instance MonadPlus Parser where
+   mzero          = Parser (\cs -> [])
+   p `mplus` q    = Parser (\cs -> parse p cs ++ parse q cs)
+
+-- Other parsing primitives: -----------------------------------------
+
+parse           :: Parser a -> String -> [(a,String)]
+parse (Parser p) = p
+
+item            :: Parser Char
+item             = Parser (\cs -> case cs of
+                                     ""     -> []
+                                     (c:cs) -> [(c,cs)])
+
+sat             :: (Char -> Bool) -> Parser Char
+sat p            = do {c <- item; if p c then return c else mzero}
+
+-- Efficiency improving combinators: ---------------------------------
+
+force           :: Parser a -> Parser a
+force p          = Parser (\cs -> let xs = parse p cs in
+                              (fst (head xs), snd (head xs)) : tail xs)
+
+(+++)           :: Parser a -> Parser a -> Parser a
+p +++ q          = Parser (\cs -> case parse (p `mplus` q) cs of
+                                     []     -> []
+                                     (x:xs) -> [x])
+
+-- Recursion combinators: --------------------------------------------
+
+string          :: String -> Parser String
+string ""        = return ""
+string (c:cs)    = do {char c; string cs; return (c:cs)}
+
+many            :: Parser a -> Parser [a]
+many p           = force (many1 p +++ return [])
+
+many1           :: Parser a -> Parser [a]
+many1 p          = do {a <- p; as <- many p; return (a:as)}
+
+sepby           :: Parser a -> Parser b -> Parser [a]
+p `sepby` sep    = (p `sepby1` sep) +++ return []
+
+sepby1          :: Parser a -> Parser b -> Parser [a]
+p `sepby1` sep   = do {a <- p; as <- many (do {sep; p}); return (a:as)}
+
+chainl          :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chainl p op a    = (p `chainl1` op) +++ return a
+
+chainl1         :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op   = do {a <- p; rest a}
+                   where
+                      rest a = do {f <- op; b <- p; rest (f a b)}
+                               +++ return a
+
+-- Useful parsers: ---------------------------------------------------
+
+char            :: Char -> Parser Char
+char c           = sat (c ==)
+
+digit           :: Parser Int
+digit            = do {c <- sat isDigit; return (ord c - ord '0')}
+
+lower           :: Parser Char
+lower            = sat isLower
+
+upper           :: Parser Char
+upper            = sat isUpper
+
+letter          :: Parser Char
+letter           = sat isAlpha
+
+alphanum        :: Parser Char
+alphanum         = sat isAlphaNum
+
+symb            :: String -> Parser String
+symb cs          = token (string cs)
+
+ident           :: [String] -> Parser String
+ident css        = do cs <- token identifier
+                      guard (not (elem cs css))
+                      return cs
+
+identifier      :: Parser String
+identifier       = do {c <- lower; cs <- many alphanum; return (c:cs)}
+
+nat             :: Parser Int
+nat              = token natural
+
+natural         :: Parser Int
+natural          = digit `chainl1` return (\m n -> 10*m + n)
+
+int             :: Parser Int
+int              = token integer
+
+integer         :: Parser Int
+integer          = do {char '-'; n <- natural; return (-n)} +++ nat
+
+-- Lexical combinators: ----------------------------------------------
+
+space           :: Parser String
+space            = many (sat isSpace)
+
+token           :: Parser a -> Parser a
+token p          = do {a <- p; space; return a}
+
+apply           :: Parser a -> String -> [(a,String)]
+apply p          = parse (do {space; p})
