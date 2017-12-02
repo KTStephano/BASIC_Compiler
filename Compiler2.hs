0dbc5d2a31f68b976c02cdc5eb32e0074e82e7ab
Justin Hall <justin.starcraft@aol.com>
2017-12-02 02:27:14 -0700

    Compiler2 is sort of working...

--- Compiler2.hs
+++ Compiler2.hs
@@ -2,13 +2,39 @@ module Compiler2
 (Sexpr(Symbol, Number, Floating, Nil, Cons), 
  Value(VIntegral, VFloating, VString, VSymbol, VBool, VStatement, Null), 
  Bytecode(End, Push, Print, PrintBang, Add, Mult, Sub, Div, Load, Store, Input, Equal, NotEqual, Greater, GEqual, Less, LEqual, IfThen, Goto), 
- analyze, car, cdr, compile, printBytecode) where
+ analyze, car, cdr, printBytecode) where
 
 import Parselib
 import Data.Char
 import Control.Monad
 import Control.Applicative hiding (many)
 import System.IO.Unsafe
+import Control.Monad.State
+import Control.Monad.Trans.State
+
+foo = "(define foo " ++
+      "'((100 input \"What is the value of A\" a )" ++
+      " (110 input \"What is the value of B\" b )" ++
+      " (120 input \"What is the value of C\" c )" ++
+      " (130 let d = ((b * b) - (4.0 * (a * c))) )" ++
+      " (140 print d) (150 end)))"
+
+quadratic1 = "(define quadratic1 '(" ++
+    "(100 input \"What is the value of A\" a )" ++
+    "(110 input \"What is the value of B\" b )" ++
+    "(120 input \"What is the value of C\" c )" ++
+    "(130 let d = ((b * b) - (4.0 * (a * c))) )" ++
+    "(140 if (d < 0) then 230 )" ++ 
+    "(150 let i = 0 )" ++ 
+    "(160 let s = 1 )" ++
+    "(170 let s = ((s + (d / s)) / 2.0) )" ++
+    "(180 let i = (i + 1) )" ++
+    "(190 if (i < 10) then 170 )" ++
+    "(200 print \"The 1st root is: \" (((-1.0 * b) + s) / (2.0 * a)) )" ++ 
+    "(210 print \"The 2nd root is: \" (((-1.0 * b) - s) / (2.0 * a)) )" ++ 
+    "(220 end )" ++
+    "(230 print \"Imaginary roots.\" )" ++
+    "(240 end )))"
 
 quadratic2 = "(define quadratic2 '(" ++
     "(100 input \"What is the value of A\" a )" ++
@@ -183,93 +209,227 @@ analyze str = let result = p str
                 (Symbol s) -> Symbol s -- Parser returned an error
                 s -> car $ cdr $ cdr s
 
-extractQuotes xs = filter (\s -> s /= '\"') xs
-
-generatePush line (Floating d) = [Push line (VFloating d)]
-generatePush line (Number i) = [Push line (VIntegral i)]
--- This is a string constant, such as "hello"
-generatePush line (Symbol s@('"':xs)) = [Push line (VString (extractQuotes s))]
--- This is probably a variable
-generatePush line (Symbol s) = [Push line (VString s), Load line]
-
--- Parses the Let statement
-evalLetArgs :: Int -> Sexpr -> [Bytecode]
-evalLetArgs line (Cons (Symbol "=") s) = evalExpr line s
-evalLetArgs line (Cons x@(Symbol s) s') = [Push line (VString s)] ++ evalLetArgs line s'
-
--- Returns bytecode that pushes a VStatement onto the stack for later execution
-evalStatement :: Int -> Sexpr -> [Bytecode]
-evalStatement line (Cons (Number i) Nil) = [Push line (VStatement [Push line (VIntegral i), Goto line])]
-evalStatement line s = [Push line $ VStatement $ evalExpr line s]
-
-evalExpr :: Int -> Sexpr -> [Bytecode]
-evalExpr line (Cons (Symbol "end") s) = [End line]
-evalExpr line (Cons (Symbol "print") s) = evalExpr line s ++ [Print line]
-evalExpr line (Cons (Symbol "print!") s) = evalExpr line s ++ [PrintBang line]
-evalExpr line (Cons (Symbol "+") s) = evalExpr line s ++ [Add line]
-evalExpr line (Cons (Symbol "-") s) = evalExpr line s ++ [Sub line]
-evalExpr line (Cons (Symbol "*") s) = evalExpr line s ++ [Mult line]
-evalExpr line (Cons (Symbol "/") s) = evalExpr line s ++ [Div line]
-evalExpr line (Cons (Symbol "=") s) = evalExpr line s ++ [Equal line]
-evalExpr line (Cons (Symbol "<>") s) = evalExpr line s ++ [NotEqual line]
-evalExpr line (Cons (Symbol ">") s) = evalExpr line s ++ [Greater line]
-evalExpr line (Cons (Symbol "<") s) = evalExpr line s ++ [Less line]
-evalExpr line (Cons (Symbol "<=") s) = evalExpr line s ++ [LEqual line]
-evalExpr line (Cons (Symbol ">=") s) = evalExpr line s ++ [GEqual line]
-evalExpr line (Cons (Symbol "then") s) = evalStatement line s ++ [IfThen line]
-evalExpr line (Cons (Symbol "if") s) = evalExpr line s
-evalExpr line (Cons (Symbol "goto") s) = evalExpr line s ++ [Goto line]
-evalExpr line (Cons (Symbol "gosub") s) = evalExpr line s ++ [NextLine line] ++ [PushCallstack line] ++ [Goto line]
-evalExpr line (Cons (Symbol "return") s) = [PopCallstack line] ++ [Goto line]
---evalExpr line (Cons (Cons (Symbol "let") s) s') = evalLetArgs line s' ++ [Let line]
-evalExpr line (Cons (Symbol "let") s) = evalLetArgs line s ++ [Store line]
-evalExpr line (Cons (Symbol "input") s) = evalExpr line s ++ [Input line]
-evalExpr line (Cons x@(Number i) s) = generatePush line x ++ evalExpr line s
-evalExpr line (Cons x@(Floating f) s) = generatePush line x ++ evalExpr line s
-evalExpr line (Cons x@(Symbol xs) s) = generatePush line x ++ evalExpr line s -- Symbol containing a quoted value
---evalExpr line (Cons (Cons i s) s') = [generatePush i] ++ evalExpr line s ++ evalExpr line s'
-evalExpr line (Cons s s') = evalExpr line s ++ evalExpr line s'
-evalExpr line _ = []
-
-compile :: Sexpr -> [Bytecode]
-compile (Cons (Number i) s) = evalExpr i s -- For this one, (Number i) is the line number and s contains the function + args
-compile (Cons s1 s2) = (compile s1) ++ (compile s2)
-compile _ = []
-
 printBytecode :: [Bytecode] -> IO ()
 printBytecode [] = putStr ""
 printBytecode (x:xs) = do
     putStrLn (show x)
     printBytecode xs
 
-data Basic = Statement {getLine' :: Int, getName :: String, body :: Basic} | Expression Sexpr deriving (Eq)
+data Basic = String' String | Integer' Int | Floating' Double | ID String | StatementList [Basic] |
+             Line Int Basic | Lines [Basic] | Variable Basic | Function String Basic | Value Basic | 
+             Statement {getName :: String, body :: Basic} | 
+             Expression String Basic | ExpressionList [Basic] | None deriving (Eq)
 
 instance Show Basic where
-    show (Statement l n b) = show l ++ ": " ++ "{" ++ n ++ " -> " ++ show b ++ "}"
-    show (Expression e) = show e
+    show (String' s) = s
+    show (Integer' i) = show i
+    show (Floating' f) = show f
+    show (ID s) = "ID " ++ s
+    show (StatementList xs) = show xs
+    show (Line l b) = show l ++ ": " ++ show b
+    show (Variable b) = "Var " ++ show b
+    show (Function s b) = "Function " ++ s ++ " -> " ++ show b
+    show (Statement s b) = "{Statement " ++ show s ++ " -> " ++ show b ++ "}"
+    show (Expression s b) = "{Expression " ++ show s ++ " -> " ++ show b ++ "}"
+    show (ExpressionList xs) = show xs
+    show None = "None"
+
+type ParseTree a = StateT Sexpr Maybe a
+
+item' :: ParseTree Sexpr
+item' = StateT $ \s -> if s == Nil then Nothing else 
+    case (car s) of
+        Nil -> Nothing
+        s' -> Just (s', cdr s)
+
+string' cs = do {i <- item'; if (Symbol cs) == i then return i else mzero}
+
+symb' :: Sexpr -> ParseTree Sexpr
+symb' cs = do {i <- item'; if cs == i then return i else mzero}
+
+-- Functions for converting a Sexpr (parse tree) into Basic
+iD = do
+    (Symbol s) <- item'
+    if (s !! 0) == '"' then mzero
+    else return $ ID s
+
+basicString =
+    do
+        (Symbol s@('"':xs)) <- item'
+        return $ String' s
+
+statements :: ParseTree Basic
+statements =
+    (do
+        s <- statement
+        string' ":"
+        (StatementList ss) <- statements
+        return $ StatementList ([s] ++ ss)) `mplus`
+    (do
+        s <- statement
+        return $ StatementList [s])
+
+statement :: ParseTree Basic
+statement = end `mplus` goto `mplus` if' `mplus` input `mplus` let' `mplus` print' `mplus` return' `mplus` setVar
+
+end :: ParseTree Basic
+end = do
+    (Symbol e) <- string' "end"
+    return $ Statement e None
+
+goto :: ParseTree Basic
+goto = do
+    (Symbol e) <- (string' "goto" `mplus` string' "gosub")
+    (Number i) <- item'
+    return $ Statement e $ Integer' i
+
+if' :: ParseTree Basic
+if' = do
+    (Symbol i) <- string' "if"
+    e <- expression
+    t <- then'
+    return $ Statement i $ ExpressionList [e, t]
+
+then' :: ParseTree Basic
+then' = do
+    (Symbol t) <- string' "then"
+    e <- expression
+    return $ Statement t e
+
+input :: ParseTree Basic
+input = do
+    (Symbol i) <- string' "input"
+    (Symbol s) <- item'
+    var <- iD
+    return $ Statement i $ ExpressionList [String' s, var]
+
+let' :: ParseTree Basic
+let' = do
+    (Symbol l) <- string' "let"
+    v <- variable
+    string' "="
+    e <- expression
+    return $ Statement l $ ExpressionList [v, e]
+
+print' :: ParseTree Basic
+print' = (do
+    (Symbol p) <- string' "print"
+    es <- expressionList
+    return $ Statement p es) `mplus`
+    (do
+        (Symbol p) <- string' "print"
+        t <- tab
+        (ExpressionList es) <- expressionList
+        return $ Statement p $ ExpressionList ([t] ++ es))
+
+return' :: ParseTree Basic
+return' = do
+    (Symbol r) <- string' "return"
+    return $ Expression r None
 
 
-variable :: Int -> Sexpr -> [Basic]
-variable line s = translate (Cons (Number line) (Cons (Symbol "let") s))
+tab :: ParseTree Basic
+tab = do
+    (Symbol t) <- string' "tab"
+    e <- expression
+    return $ Statement t e
 
-translate :: Sexpr -> [Basic]
-translate (Cons (Number i) c@(Cons (Symbol s) s')) = if s `elem` statementDeclarations then [Statement i s $ Expression s']
-                                                   else variable i c
-translate (Cons s s') = translate s ++ translate s'
-translate Nil = []
+setVar :: ParseTree Basic
+setVar = do
+    v <- variable
+    string' "="
+    e <- expression
+    return $ Statement "let" $ ExpressionList [v, e]
+
+optimization = StateT $ \s -> if s == Nil then Nothing else
+    case s of
+        (Cons c@(Cons s s') s'') ->
+            case (runStateT expression c) of
+                Nothing -> Nothing
+                Just (res, _) -> Just (res, s'')
+        _ -> Nothing
+
+expression :: ParseTree Basic
+expression = (do
+        a <- andExp
+        (Symbol o) <- string' "or"
+        e <- expression
+        return $ Expression o $ ExpressionList [a, e]) `mplus` andExp
+
+expressionList :: ParseTree Basic
+--expressionList = (do {e <- expression; (ExpressionList es) <- expressionList; return $ ExpressionList ([e] ++ es)}) `mplus`
+                 --(do {e <- expression; return $ ExpressionList [e]})
+
+expressionList =
+    (do
+        e <- expression
+        (ExpressionList es) <- expressionList
+        return $ ExpressionList ([e] ++ es)) `mplus`
+    (do
+        e <- expression
+        return $ ExpressionList [e])
+
+andExp =
+    (do
+        n <- notExp
+        (Symbol a) <- string' "and"
+        ae <- andExp
+        return $ Expression a $ ExpressionList [n, ae]) `mplus` notExp
+
+notExp =
+    (do
+        (Symbol n) <- string' "not"
+        c <- compareExp
+        return $ Expression n c) `mplus` compareExp
+
+compareExp = (do {a <- addExp; (Symbol equ) <- string' "="; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             (do {a <- addExp; (Symbol equ) <- string' "<>"; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             (do {a <- addExp; (Symbol equ) <- string' ">"; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             (do {a <- addExp; (Symbol equ) <- string' ">="; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             (do {a <- addExp; (Symbol equ) <- string' "<"; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             (do {a <- addExp; (Symbol equ) <- string' "<="; c <- compareExp; return $ Expression equ $ ExpressionList [a, c]}) `mplus`
+             addExp
+
+addExp :: ParseTree Basic
+addExp = (do {m <- multExp; (Symbol op) <- string' "+"; a <- addExp; return $ Expression op $ ExpressionList [m, a]}) `mplus`
+         (do {m <- multExp; (Symbol op) <- string' "-"; a <- addExp; return $ Expression op $ ExpressionList [m, a]}) `mplus`
+         multExp
+
+multExp :: ParseTree Basic
+multExp = (do {n <- negateExp; (Symbol op) <- string' "*"; m <- multExp; return $ Expression op $ ExpressionList [n, m]}) `mplus`
+          (do {n <- negateExp; (Symbol op) <- string' "/"; m <- multExp; return $ Expression op $ ExpressionList [n, m]}) `mplus`
+          negateExp
+
+negateExp :: ParseTree Basic
+negateExp = (do {(Symbol op) <- string' "-"; p <- powerExp; return $ Expression op $ ExpressionList [p]}) `mplus`
+            powerExp
+
+powerExp = (do {v <- value; (Symbol op) <- string' "^"; p <- powerExp; return $ Expression op $ ExpressionList [v, p]}) `mplus`
+           value
+
+value = variable `mplus` function `mplus` constant `mplus` optimization
+
+variable = iD
+
+function = (do {(Symbol i) <- string' "int"; e <- expression; return $ Function i e})
+
+constant = (do {(Number i) <- item'; return $ Integer' i}) `mplus`
+           (do {(Floating f) <- item'; return $ Floating' f}) `mplus`
+           basicString
 
 --renumber :: [Basic] -> ([Basic], [(Int, Int)])
+{-
 renumber [] _ _ mapping = ([], mapping)
 renumber basic@((Statement l n b):xs) line newline mapping = if l /= line then renumber basic l (newline + 1) mapping
                                                              else let s = [Statement newline n b]
                                                                       (ys, mapping') = renumber xs line newline mapping
                                                                   in if ((l, newline) `elem` mapping') then (s, mapping')
                                                                      else (s ++ ys, [(l, newline)] ++ mapping')
+-}
 
-printBasic [] = putStr ""
-printBasic (x:xs) = do
-    putStrLn (show x)
-    printBasic xs
-
-statementDeclarations = ["dim", "end", "for", "goto", "gosub", "if", "input", "let", "next", "print", "print!", "rem", "return"]
-
+translateSexpr Nil = []
+translateSexpr (Cons (Number i) s) = let result = runStateT statements s
+                                     in case result of
+                                        (Just (r@(StatementList ss), _)) -> [Line i r]
+                                        Nothing -> []
+translateSexpr (Cons s s') = translateSexpr s ++ translateSexpr s'
