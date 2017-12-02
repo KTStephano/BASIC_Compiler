1eea738179b835da4d6fe0b2ca7010b25822c77e
tmanzanares <tmanzanares@unm.edu>
2017-12-02 15:45:29 -0700

    Works with IfThen and Goto
    Still working on CallStack and NextLine

--- BasicInterpreter.hs
+++ BasicInterpreter.hs
@@ -1,9 +1,3 @@
--- * = 0 or mroe
--- + = 1 or more
--- {} = symbolic class or something
--- lower = lowercase digit
--- digit = number
-
 import Parselib
 import Compiler
 import Data.Char
@@ -29,6 +23,23 @@ pr3 = "(define pr '((100 print ((1 + 4) + (2 + 3)))))"
 lettest = "(define ltest '((100 let x = ((2 * 3) - (4 * (5 * 6))))" ++
                           "(200 print x)))"
 
+
+letters = "(define ltest '((100 let x = ((2 * 3) - 4))" ++
+                          "(200 print x)" ++
+                          "(300 if (x > 0) then 500)" ++
+                          "(400 print 1000)" ++ 
+                          "(500 print 10)))"
+
+goCheck = "(define gotest '((100 print 5)" ++
+                           "(200 print 10)" ++
+                           "(300 goto 500)" ++
+                           "(400 print 100000)" ++
+                           "(500 print 1)))"
+
+                           --"(300 if (x > 0) then goto 500)" ++
+                          -- "(300 print 100)" ++
+                          -- "(500 if (x < 0) goto 400)))"
+
 quadratic1 = "(define quadratic1 '(" ++
     "(100 input \"What is the value of A\" a )" ++
     "(110 input \"What is the value of B\" b )" ++
@@ -46,6 +57,23 @@ quadratic1 = "(define quadratic1 '(" ++
     "(230 print \"Imaginary roots.\" )" ++
     "(240 end )))"
 
+quadratic2 = "(define quadratic2 '(" ++
+  "(100 input \"What is the value of A\" a )" ++
+  "(110 input \"What is the value of B\" b )" ++
+  "(120 input \"What is the value of C\" c )" ++
+  "(130 let d = ((b * b) - (4.0 * (a * c))) )" ++
+  "(140 if (d < 0) then 190 )" ++
+  "(150 gosub 210 )" ++
+  "(160 print \"The 1st root is: \" (((-1.0 * b) + s) / (2.0 * a)) )" ++
+  "(170 print \"The 2nd root is: \" (((-1.0 * b) - s) / (2.0 * a)) )" ++
+  "(180 end )" ++
+  "(190 print \"Imaginary roots.\" )" ++
+  "(200 end )" ++
+  "(210 let s = 1 )"  ++
+  "(220 for i = 1 to 10 )" ++
+  "(230 let s = ((s + (d / s)) / 2.0) )" ++
+  "(240 next i )" ++
+  "(250 return )))" 
 
 newtype Environment = Environment {getEnv :: [(String, Value)]} deriving (Show, Eq)
 
@@ -139,16 +167,22 @@ input frame env = let (frame', (VSymbol var _)) = pop frame
 
 
 --print' (Frame []) = "\n"
+print' :: Frame -> IO()
 print' (Frame []) = putStrLn ""
 print' (Frame (x:xs)) = do
     putStr (show x)
     print' (Frame xs)
 
+printBang :: Frame -> IO()
 printBang (Frame []) = putStr ""
 printBang (Frame (x:xs)) = do
     putStr (show x)
     printBang (Frame xs)
 
+--nextline is neecd for go sub
+--same but adition of callstack
+--callstack is another frame
+--start as emput but popcallstacck,pushcallstck, then change our focus rto callstack frame
 vm :: [Bytecode] -> Environment -> [Bytecode] -> Frame -> IO ()
 vm program env [] frame = putStr ""
 vm program env ((End l):rest) frame = putStr ""
@@ -182,6 +216,7 @@ vm program env ((NotEqual l):rest) frame = do
     vm program env rest (push frame' val)
 vm program env ((Greater l):rest) frame = do
     let (frame', val) = logical (>) frame
+    --print (frame', val)
     vm program env rest (push frame' val)
 vm program env ((Less l):rest) frame = do
     let (frame', val) = logical (<) frame
@@ -192,9 +227,50 @@ vm program env ((GEqual l):rest) frame = do
 vm program env ((LEqual l):rest) frame = do
     let (frame', val) = logical (<=) frame
     vm program env rest (push frame' val)
+vm program env ((IfThen l):rest) frame = do
+    let (frame',res) = getStatement frame
+    vm program env (res ++ rest) frame'    
+vm program env ((Goto l):rest) frame = do
+  let (frame',num) = getLineNum frame
+      newRest = subProgram program num
+  vm program env newRest frame'
 vm program env ((PrintBang l):rest) frame = do
     printBang frame
     vm program env rest (Frame [])
 vm program env ((Print l):rest) frame = do
     print' frame
     vm program env rest (Frame [])
+
+--This should return the rest of the programs byte code
+--findLine program line = line
+
+subProgram [] _ = []
+subProgram (x:xs) l = if line x == l then x:(subProgram xs l) else subProgram xs l
+
+getLineNum frame = let (frame', VIntegral line) = unary frame in (frame', line)
+
+getStatement frame = let (frame',((VBool x),(VStatement ys))) = binary frame in
+                if x then (frame', ys)
+                else (frame', [])
+
+
+
+{-
+stuff that doesnt work but might mess with
+{-
+getIt frame = let (frame', (x, y)) = binary frame
+                      in case (x, y) of
+                           (VBool True,_) -> (frame', y)
+                           (VBool False,_) -> (frame',x) -- []
+-}
+{-
+--vm program env ((IfThen l):rest) frame = do
+--  let (frame',result) = getLine env frame
+--  vm program env rest (push frame' result)
+-- (Double -> Double -> Bool) -> Frame -> (Frame, Value)
+getLine env frame = let (frame', (x, y)) = binary frame
+                      in case (x, y) of
+                           (True,func) -> findEnv env "130"
+                           (False,func) -> findEnv env "100"
+-}
+-}
